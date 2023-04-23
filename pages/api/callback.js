1006032f1dc9e5ac1b634f3cba2eafd09262088c
Shreyansh Jain <shreyanshjain14072002@gmail.com>
2023-04-23 20:51:34 +0530

    Update callback.js

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,5 +1,11 @@
 const TasksMap = {
-    'Aditya Kumar Garg': 'https://google.com'
+    'Aditya Kumar Garg': 'https://youtube.com/shorts/i7AWFvfx1ho?feature=share',
+    'AV Jefferson': 'https://open.spotify.com/track/5aszL9hl6SBzFNsOvw8u8w?si=FzArQigNQQm5OdalVb6wOg',
+    'Ayush Tripathi': 'https://youtu.be/zhwwmdh2oyc',
+    'Surya Prakash Mishra': 'https://youtu.be/FzG4uDgje3M',
+    'Swaraj Jain': 'https://youtu.be/sONw3dihCRs',
+    'Anshuman Tejaswi': 'https://youtube.com/shorts/afwSiJuiUzU?feature=share',
+    'CVSS Santosh': 'https://youtu.be/JALa8dhG6dA'
 }
 
 
